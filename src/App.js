f30252fe6279b13ed1a6943a5370de902b13931a
guybracha <guy.bracha@gmail.com>
2024-04-01 13:49:36 +0300

    new message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './comp/Counter';
+import Message from './comp/Message';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-danger'>React Work</h1>
+      <Message/>
+      <hr/>
       <Counter/>
     </div>
   );
--- src/comp/Counter.js
+++ src/comp/Counter.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Counter() {
+    const[Counter, setCounter] = useState(4);
+
+    const onAdd1 = () => {
+        setCounter(Counter + 1);
+    }
   return (
     <div>
-      Counter
+      <h2>Counter: {Counter}</h2>
+      <button onClick={onAdd1}>Add 1</button>
+      <button onClick={()=>{
+        setCounter(Counter -1)
+      }}>Reduce 1</button>
     </div>
   )
 }
