892774a9cedd9621af2f857f5d760ab5ec3f337b
guybracha <guy.bracha@gmail.com>
2024-04-01 14:12:42 +0300

    new additions

--- src/comp/Value.js
+++ src/comp/Value.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function Value() {
-    
+    const nameRef = useRef();
+    const [phoneVal, setPhoneVal] = useState("");
+
     const onSub = (e) => {
         e.preventDefault()
+        console.log(nameRef.current.value);
+        console.log(phoneVal);
     }
 
   return (
     <div>
       <form onSubmit={onSub} className='col-md-6'>
         <label>Name:</label>
-        <input type='text' className='form-control mb-3'></input>
+        <input ref={nameRef} type='text' className='form-control mb-3'></input>
         <label>Phone:</label>
-        <input type='tel' className='form-control'></input>
+        <input onChange={(e)=> setPhoneVal(e.currentTarget.value)} type='tel' className='form-control'></input>
         <button className='btn btn-info mt-3'>Send</button>
       </form>
     </div>
